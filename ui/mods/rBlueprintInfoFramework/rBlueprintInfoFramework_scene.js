91f9ebbf739acc63a9c2f58ad7c41ec24009f0d5
Raevn <d_bened@raevn.com>
2014-09-27 10:45:15 +1000

    Initial commit of version 1.4.3

---
+++ ui/mods/rBlueprintInfoFramework/rBlueprintInfoFramework_scene.js
@@ -0,0 +1,17 @@
+//===================================================
+// rBlueprintInfoFramework - Blueprint Info Framework
+//---------------------------------------------------
+// rBlueprintInfoFramework_scene.js
+// Created by Raevn
+// Version 1.4.3 (2014/09/21)
+//---------------------------------------------------
+
+model.BIFReady = ko.observable(bif.getBIFReady());
+
+bif.registerBIFReadyCallback(function() {
+	model.BIFReady(true);
+});
+
+if (model.BIFReady() == false) {
+	bif.initialiseBIF(false, true);
+}
